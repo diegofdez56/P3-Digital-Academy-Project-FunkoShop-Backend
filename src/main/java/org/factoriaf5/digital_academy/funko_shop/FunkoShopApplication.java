37cede5da7119bf8315ec8d16a4e51756c04145a
Miharu669 <veronicadoelfuentes@gmail.com>
2024-09-12 11:47:59 +0200

    first commit

---
+++ src/main/java/org/factoriaf5/digital_academy/funko_shop/FunkoShopApplication.java
@@ -0,0 +1,13 @@
+package org.factoriaf5.digital_academy.funko_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunkoShopApplication.class, args);
+	}
+
+}
