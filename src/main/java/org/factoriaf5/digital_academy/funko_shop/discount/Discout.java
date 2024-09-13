cdde56eaf906927a70dacf78d9e225b26857b199
Bego Blanco <begoblanfer@gmail.com>
2024-09-13 12:19:53 +0200

    discount model and repository

---
+++ src/main/java/org/factoriaf5/digital_academy/funko_shop/discount/Discout.java
@@ -0,0 +1,25 @@
+package org.factoriaf5.digital_academy.funko_shop.discount;
+
+import org.factoriaf5.digital_academy.funko_shop.product.Product;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "discounts")
+public class Discout {
+    
+    @Id
+    @GeneratedValue
+    @Column(name = "discount_id")
+    private Long id;
+    private int percentage;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "product_id")
+    private Product product;
+}
