f64b6c38d71aa8eb3f498c941b158b3463493cd9
Miharu669 <veronicadoelfuentes@gmail.com>
2024-09-13 10:09:37 +0200

    added Role Repository

---
+++ src/main/java/org/factoriaf5/digital_academy/funko_shop/roles/RoleRepository.java
@@ -0,0 +1,7 @@
+package org.factoriaf5.digital_academy.funko_shop.roles;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    
+}
