1a95a501eab2b779ab99acedf5320780a7748414
devjmv <javier161890@gmail.com>
2024-09-17 11:07:07 +0200

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,7 +22,8 @@ INSERT INTO categories (category_id, name) VALUES (default, 'Animación'),
  (default, 'DC Comics');
 
  -- Products
- INSERT INTO products (product_id, name, imageHash, description, price, stock, is_available, category_id, discount_id) VALUES(default, 'asdasd', 'asdasd','asdas,' 10, 10, true, 'asdasd', 1, 0);
+ INSERT INTO products (product_id, name, image, description, price, stock, is_available, category_id, discount_id) 
+                VALUES (default, 'asdasd', 'asdasd','asdas', 10, 10, true, 1, null);
 
 
 
