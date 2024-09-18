daea165c7dc92dcc58e0b655b759f517878556e4
devjmv <javier161890@gmail.com>
2024-09-18 10:34:40 +0200

    updated User and add role and permission

--- src/main/java/org/factoriaf5/digital_academy/funko_shop/user/User.java
+++ src/main/java/org/factoriaf5/digital_academy/funko_shop/user/User.java
@@ -1,14 +1,16 @@
 package org.factoriaf5.digital_academy.funko_shop.user;
 
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import org.factoriaf5.digital_academy.funko_shop.account_settings.AccountSettings;
 import org.factoriaf5.digital_academy.funko_shop.cart.Cart;
 import org.factoriaf5.digital_academy.funko_shop.order.Order;
 import org.factoriaf5.digital_academy.funko_shop.profile.Profile;
 import org.factoriaf5.digital_academy.funko_shop.review.Review;
-import org.factoriaf5.digital_academy.funko_shop.role.Role;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -19,23 +21,29 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     private long id;
+
     @NonNull
     private String email;
+
     @NonNull
     private String password;
 
-    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Profile profile;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    Set<Role> roles;
+    /*
+     * @ManyToMany(fetch = FetchType.EAGER)
+     * 
+     * @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"),
+     * inverseJoinColumns = @JoinColumn(name = "role_id"))
+     * Set<Role> roles;
+     */
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Review> reviews;
@@ -48,5 +56,43 @@ public class User {
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private Cart cart;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
