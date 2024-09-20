65fd5e979114bd6da02d6e9d0e0188eda4a43d73
Vero Doel <veronicadoelfuentes@gmail.com>
2024-09-20 11:31:54 +0200

    fixed cors

--- src/main/java/org/factoriaf5/digital_academy/funko_shop/config/SecurityConfiguration.java
+++ src/main/java/org/factoriaf5/digital_academy/funko_shop/config/SecurityConfiguration.java
@@ -74,14 +74,15 @@ public class SecurityConfiguration {
         }
 
         @Bean
-    public CorsConfigurationSource corsConfiguration() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        public CorsConfigurationSource corsConfiguration() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowCredentials(true);
+            configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+            configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type","Accept", "X-Requested-With"));
+            configuration.setExposedHeaders(Arrays.asList("Authorization"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
+        }
 }
