741e7001b629f6da4acb59d7ca0279b7415d481e
Miharu669 <veronicadoelfuentes@gmail.com>
2024-09-23 10:59:02 +0200

    fixed sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,7 @@
 -- Users
-INSERT INTO users (user_id, email, password) VALUES (default, 'user@gmail.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
-INSERT INTO users (user_id, email, password) VALUES (default, 'admin@gmail.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO');
+INSERT INTO users (user_id, email, password, role) VALUES (default, 'user@gmail.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO', 'USER');
+INSERT INTO users (user_id, email, password, role) VALUES (default, 'admin@gmail.com', '$2a$12$8LegtLQWe717tIPvZeivjuqKnaAs5.bm0Q05.5GrAmcKzXw2NjoUO', 'ADMIN');
 
--- Roles_Users (Ensure role_id and user_id are correctly mapped)
-INSERT INTO user_roles (role_id, user_id) VALUES (1, 1);  -- Assign 'ROLE_USER' to user 'pepe'
-INSERT INTO user_roles (role_id, user_id) VALUES (2, 2);  -- Assign 'ROLE_ADMIN' to user 'pepa'
 
 -- Profiles
 INSERT INTO profiles (first_name, last_name, phone_number, street, city, region, postal_code, country, user_id, is_shipping, is_subscribed) VALUES
@@ -19,7 +16,8 @@ INSERT INTO categories (category_id, name) VALUES (default, 'Animación'),
 
  -- Products
  INSERT INTO products (product_id, name, image, description, price, stock, is_available, category_id, discount_id) 
-                VALUES (default, 'asdasd', 'asdasd','asdas', 10, 10, true, 1, null);
+                VALUES (default, 'asdasd', 'asdasd','asdas', 10, 10, true, 1, null)
+                ;
 
 -- Orders
 INSERT INTO orders (order_id, status, total_price, total_items, is_paid, user_id) VALUES
@@ -44,10 +42,10 @@ INSERT INTO discounts (discount_id, percentage, is_active) VALUES
 
 --  OrderItems
 INSERT INTO order_items (order_item_id, quantity, order_id, product_id)
-VALUES (1, 2, 1, 1);
+VALUES (default, 2, 1, 1);
 INSERT INTO order_items (order_item_id, quantity, order_id, product_id)
-VALUES (2, 3, 1, 2);
+VALUES (default, 3, 1, 1);
 
 -- Reviews
-INSERT INTO reviews (review_id, rating, order_item_id, user_id) VALUES (1, 5, 1, 1);
-INSERT INTO reviews (review_id, rating, order_item_id, user_id) VALUES (2, 4, 2, 1);
+INSERT INTO reviews (review_id, rating, order_item_id, user_id) VALUES (default, 5, 1, 1);
+INSERT INTO reviews (review_id, rating, order_item_id, user_id) VALUES (default, 4, 2, 1);
